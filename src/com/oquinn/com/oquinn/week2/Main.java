48b7c054fc90706378fe7341bcce64e249113aca
JerrettOQ <jeo.homebrew.way@gmail.com>
2018-01-23 13:11:32 -0500

    first commit

---
+++ src/com/oquinn/com/oquinn/week2/Main.java
@@ -0,0 +1,14 @@
+package com.oquinn.com.oquinn.week2;
+
+/**
+ * Created by 008366 on 1/23/2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+        System.out.println("Welcome to week2!");
+    }
+}
